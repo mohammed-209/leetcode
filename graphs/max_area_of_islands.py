7ae12c969532e56ea94b410d7bd6b0c20b0cf402
mohammed-209 <msalehh209@gmail.com>
2025-10-03 23:17:14 -0700

    solved 695. Max Area of Island

---
+++ graphs/max_area_of_islands.py
@@ -0,0 +1,21 @@
+# 695. Max Area of Island
+# https://leetcode.com/problems/max-area-of-island/ 
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        
+        def dfs(r, c):
+            if (r, c) in visited or r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0:
+                return 0
+            visited.add((r, c))
+            return (1+ dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+
+        
+        return area
